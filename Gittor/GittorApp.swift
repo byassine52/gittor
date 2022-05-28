b1a2b3b6848f62116d1b506e43e00971e978d373
Yassine ElBadaoui <byassine52@users.noreply.github.com>
2022-05-28 17:50:04 +0900

    Initial Commit

---
+++ Gittor/GittorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GittorApp.swift
+//  Gittor
+//
+//  Created by Yassine ElBadaoui on 2022/05/28.
+//
+
+import SwiftUI
+
+@main
+struct GittorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
